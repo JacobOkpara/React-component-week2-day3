3a187e2e569841e2d8ef7ecbeae0930ace042ec0
unknown <okparajacob@gmail.com>
2022-03-16 14:32:32 +0000

    first commit

---
+++ week-1/lessons/js/app.js
@@ -0,0 +1,99 @@
+// operators
+let a = 10;
+let b = 15;
+let c = "10"
+
+// basic operator
+console.log(a + b);
+console.log(a * b);
+console.log(a - b);
+console.log(a / b);
+
+// remainder
+console.log(a % b);
+
+//urinary operator
+console.log(a++);
+console.log(a);
+console.log(a--);
+console.log(++a);
+console.log(a);
+console.log(--a);
+
+// comparison
+console.log(a < b);
+console.log(a > b);
+console.log(a == b);
+console.log(a === c);
+
+// control flow
+// if ( condition)
+
+let score = 80;
+
+if ( score = 80) {
+    console.log("You had A")
+}   else if (score = 70) {
+    console.log("You had B+");
+} else if ( score = 65) {
+    console.log ("You had C+")
+} else if (score = 60) {
+    console.log ("You had C")
+}else if (score =55) {
+    console.log( "You had D+")
+} else if (score = 50) {
+    console.log ("You had D ")
+} else if ( score = 40) {
+    console.log("You had E")
+} else ("You fail");
+
+
+// switch case
+
+const gender = "Male"
+switch ( gender) {
+ case "Male":
+     console.log ("go to the washroom");
+     break;
+     case "Female":
+     console.log ("go to the female washroom");
+     break;
+     default:
+         conslole.log("no washroom fo you.")
+         break;
+
+}    
+
+// for ( let i = 1; i < 10; i++)
+// functions
+for ( let count = 1; count < 10; count ++ ) {
+    console.log(count);
+}
+
+
+// while - loop
+
+let count = 1;
+
+while (count < 10) {
+    console.log(count);
+    count++;
+    
+}
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ week-1/lessons/js/script.js
@@ -0,0 +1,24 @@
+let firstName = "Okpara";
+let lastName = "Jake";
+let age = 32;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+
+console.log("My name is " + firstName + " " + lastName +" I am ten years old"  );
+
+console.log(`my name is ${ firstName} ${lastName}. i am ${age} years old.`);
+
+let fruits = ["Mango", "Apple","Orange","Pineaple", "Pawpaw" , "Banana"];
+console.log(fruits);
+console.log(fruits.length);
+console.log(fruits [2]);
+console.log(fruits.push ("Groundnut")),
+console.log(fruits.pop());
+console.log(fruits.unshift ("Grape"));
+console.log(fruits.shift());
+console.log(fruits.includes("Sugarcane"));
+console.log(fruits.includes("Pawpaw"));
+console.log(fruits.join("+"));
+console.log(fruits);
