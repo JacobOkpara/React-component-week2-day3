c4ff57d47d38578742205ec604fa8a32a54dc8f5
unknown <okparajacob@gmail.com>
2022-03-22 14:42:00 +0000

    react componet

--- code-along/src/App.js
+++ code-along/src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import writers from "./writers";
+import { ProfileCard } from "./ProfileCard";
 
 function App() {
   return (
@@ -8,19 +9,7 @@ function App() {
       <h1>Writers Profiles</h1>
       <div className="container">
         {writers.map((writer) => (
-          <div className="card">
-            <img
-              src={`/images/${writer.avatar}.png`}
-              width="300px"
-              height="300px"
-              alt=""
-            />
-            <div className="textGroup">
-              <h3>{writer.name}</h3>
-              <p>{writer.email}</p>
-              <p>{writer.phone}</p>
-            </div>
-          </div>
+          <ProfileCard writer={writer} />
         ))}
       </div>
     </div>
