c86d47fd709cc0f8f8c3db7db70438196c083902
unknown <okparajacob@gmail.com>
2022-03-23 14:06:21 +0000

    react component 3

--- code-along/src/App.js
+++ code-along/src/App.js
@@ -1,9 +1,21 @@
-import logo from "./logo.svg";
-import "./App.css";
+// import logo from "./logo.svg";
+// import "./App.css";
 import writers from "./writers";
 import { ProfileCard } from "./ProfileCard";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [writers, setWriters] = useState([]);
+
+  useEffect(() => {
+    const getWriters = async () => {
+      const response = await fetch("/writers.json");
+      const data = await response.json();
+      setWriters(data);
+    };
+    getWriters();
+  }, []);
+
   return (
     <div>
       <h1>Writers Profiles</h1>
