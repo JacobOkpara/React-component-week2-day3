e6dd3f0257fb259c121cbecaf206b482764db5af
unknown <okparajacob@gmail.com>
2022-03-24 13:53:59 +0000

    react-component-4

--- code-along/src/App.js
+++ code-along/src/App.js
@@ -1,28 +1,62 @@
 // import logo from "./logo.svg";
 // import "./App.css";
 import writers from "./writers";
-import { ProfileCard } from "./ProfileCard";
+import ProfileCard from "./ProfileCard";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [writers, setWriters] = useState([]);
+  const [data, setData] = useState({
+    writers: [],
+    loading: false,
+  });
 
-  useEffect(() => {
-    const getWriters = async () => {
-      const response = await fetch("/writers.json");
-      const data = await response.json();
-      setWriters(data);
-    };
-    getWriters();
-  }, []);
+  const handleClick = () => {
+    setData((prevData) => ({
+      ...prevData,
+      loading: true,
+    }));
+
+    setTimeout(() => {
+      const getWriters = async () => {
+        const response = await fetch("/writers.json");
+        const data = await response.json();
+        setData({
+          writers: data,
+          loading: false,
+        });
+      };
+
+      getWriters();
+    }, 2000);
+  };
+
+  if (data.loading) {
+    <div>
+      <h1>Writer profiles</h1>
+      <div className="container">
+        <div className="card action">
+          <p className="infoText"> Loading ...</p>
+        </div>
+      </div>
+    </div>;
+  }
 
   return (
     <div>
-      <h1>Writers Profiles</h1>
+      <h1>Writer Profiles</h1>
       <div className="container">
-        {writers.map((writer) => (
-          <ProfileCard writer={writer} />
-        ))}
+        {data.writers.length === 0 ? (
+          <div className="card action">
+            <p className="infoText"> Oops... no writer profile found</p>
+            <button className="actionBtn" onClick={handleClick}>
+              Get Writers
+            </button>
+          </div>
+        ) : (
+          data.writers.map((writer) => (
+            <ProfileCard key={writer.id} writer={writer} />
+          ))
+        )}
       </div>
     </div>
   );
